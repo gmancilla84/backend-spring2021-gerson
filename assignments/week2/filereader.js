8c716a968ae09a3654523298a2cf05f02f2a34ba
Gerson <gersons-mbp.lan>
2021-03-30 20:03:43 -0700

    Working on FS and argv

--- assignments/week2/filereader.js
+++ assignments/week2/filereader.js
@@ -6,8 +6,10 @@ const arguments = process.argv;
 // garb third argument directly and place it into a variable
 let filename = arguments[2];
 
+// Check if the argument was empty.
 if (filename === undefined) {
 
+    // if it was empty, provide a message that is for a new person.
     console.log(`
 Welcome to my file reader! Please provide the file name you want to read after the command.
     
@@ -15,18 +17,21 @@ Example:
     node filereader.js myFile.txt
     `);
 
-    // run the script early so we don't run the code below.
+    // end the script early so we don't run the code below.
     return;
 
 }
 
+// Check if the file exist.
 if (fs.existsSync(filename)) {
 
+    // if it does exist...
     // Read the file based on the argument in the command.
     let fileContents = fs.readFileSync(filename, "utf-8");
 
     console.log(fileContents);
 
 } else {
+    // Otherwise tell the user that we couldn't find the file.
     console.log("Sorry but we couldn't find that file! Please check the file name.");
 }
